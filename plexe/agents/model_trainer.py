c9fba5db4e3d4de1c0cf0bf96f5a21711b933fc6
marcellodebernardi <marcello.logins@outlook.com>
2025-05-18 15:20:00 -0700

    fix: rename tool closures for clarity

--- plexe/agents/model_trainer.py
+++ plexe/agents/model_trainer.py
@@ -11,7 +11,7 @@ from smolagents import ToolCallingAgent, LiteLLMModel
 from plexe.internal.common.utils.agents import get_prompt_templates
 from plexe.internal.models.tools.execution import get_executor_tool
 from plexe.internal.models.tools.response_formatting import format_final_mle_agent_response
-from plexe.internal.models.tools.training import get_generate_training_code, get_fix_training_code
+from plexe.internal.models.tools.training import get_training_code_generation_tool, get_training_code_fixing_tool
 from plexe.internal.models.tools.validation import validate_training_code
 
 logger = logging.getLogger(__name__)
@@ -51,9 +51,9 @@ class ModelTrainerAgent:
             ),
             model=LiteLLMModel(model_id=ml_engineer_model_id),
             tools=[
-                get_generate_training_code(tool_model_id),
+                get_training_code_generation_tool(tool_model_id),
                 validate_training_code,
-                get_fix_training_code(tool_model_id),
+                get_training_code_fixing_tool(tool_model_id),
                 get_executor_tool(distributed),
                 format_final_mle_agent_response,
             ],
