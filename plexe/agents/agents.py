150f8188d36756c88a91dddbf90775140363845c
marcellodebernardi <marcello.logins@outlook.com>
2025-05-28 18:37:53 -0700

    fix: print statement incompatible with py311

--- plexe/agents/agents.py
+++ plexe/agents/agents.py
@@ -203,11 +203,7 @@ class PlexeAgent:
         object_registry = ObjectRegistry()
         result = self.manager_agent.run(task=task, additional_args=additional_args)
 
-        print(
-            f"Registry contents:\n\n"
-            f"{json.dumps([n.rsplit(".")[-1].replace("'", "") for n in object_registry.list()], indent=4)}"
-            f"\n\n"
-        )
+        print(f"Registry contents:\n\n" f"{json.dumps(object_registry.list(), indent=4)}" f"\n\n")
 
         try:
             # Only log the full result when in verbose mode
